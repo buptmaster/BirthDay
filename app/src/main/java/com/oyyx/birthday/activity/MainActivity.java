be920f19597763c69ea3ad256d0e078383c87fd4
masteraux1 <768471488@qq.com>
2017-09-06 23:12:34 +0800

    First Commit

---
+++ app/src/main/java/com/oyyx/birthday/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.oyyx.birthday.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.oyyx.birthday.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
